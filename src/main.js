d010866efef3fc0d8f0e24263ffcf3e903d80576
Zahra <zahrahanifi98@gmail.com>
2024-11-13 01:31:47 +0330

    Added tailwind and vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp } from 'vue';
+import './assets/css/main.css'
+import router from './router/index.js'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const index = [
+    {
+        path: '/',
+        name: 'IndexPage',
+        component: import('../pages/index.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: index,
+});
+
+export default router;
