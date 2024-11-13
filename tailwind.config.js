3cbc64011e75c0087ba93c842bb81ba213a9b820
Zahra <zahrahanifi98@gmail.com>
2024-11-14 00:30:52 +0330

    Added custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+import { colors } from './palette.js'
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
