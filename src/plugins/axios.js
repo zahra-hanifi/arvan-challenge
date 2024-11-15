da781800bccd1bcd0697566efadc1c7db7d5fae8
Zahra <zahrahanifi98@gmail.com>
2024-11-15 23:04:14 +0330

    Added authorization header to request in axios plugin

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 
+const token = localStorage.getItem('token')
+
 const axiosInstance = axios.create({
   baseURL: 'http://5.34.201.164:3000/api',
   timeout: 10000,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Token ${token}`,
+  },
 })
 
 export default axiosInstance
