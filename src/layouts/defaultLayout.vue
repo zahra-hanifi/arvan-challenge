50f0e8d4271244653ce8dba954b506be784214dc
Zahra <zahrahanifi98@gmail.com>
2024-11-15 23:06:49 +0330

    Add api call to get current user info in default layout

--- src/layouts/defaultLayout.vue
+++ src/layouts/defaultLayout.vue
@@ -1,5 +1,22 @@
 <script setup>
 import Header from '../components/general/Header.vue'
+import { getCurrentInstance, onMounted } from 'vue'
+import { useAuthStore } from '../stores/auth.js'
+
+const { proxy } = getCurrentInstance()
+const axios = proxy.$axios
+const { setToken } = useAuthStore()
+
+function getCurrentUserInfo() {
+  axios.get('/user').then((response) => {
+    const user = response.data.user
+    setToken(user)
+  })
+}
+
+onMounted(() => {
+  getCurrentUserInfo()
+})
 </script>
 
 <template>
